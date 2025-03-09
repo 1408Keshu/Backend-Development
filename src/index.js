b3ddde243ee2dfc307bc3383dd35f95c2ed8e902
Keshav Chauahan <keshavchauhan1312@gmail.com>
2025-03-09 11:57:09 +0530

    first approch to connect mongodb in mern

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-ECHO is on.
+import dotenv from "dotenv"
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+// const app = express()
+// ;( async ()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        application.on("error",(error) =>{
+//         console.log("Error:",error);
+//         throw error
+//        })
+       
+//        app.listen(process.env.PORT, ()=>{
+//         console.log(`App is listening on port ${process.env.PORT}`);
+//        })
+//     } catch (error) {
+//         console.log("Error: ",error)
+//         throw err
+//     }
+
+// })()
